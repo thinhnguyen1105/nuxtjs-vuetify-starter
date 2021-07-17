1b7f5ca07c1072150f527f176ff19910e7fe22b9
Genius Mac <Genius Mac>
2021-07-17 10:52:21 +0700

    test api

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
         <logo />
-        <div>{{ vendor }}</div>
+        <!-- <div>{{ vendor }}</div> -->
       </div>
     </v-col>
   </v-row>
@@ -15,13 +15,14 @@ export default {
   components: {
     Logo
   },
-  async asyncData({ $axios }) {
-    const vendor = await $axios.$get(
-      // `https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin`
-      'https://api.nuxtjs.dev/mountains'
-    )
-    console.log('ip', vendor)
-    return { vendor }
+  asyncData({ req, res, $axios }) {
+    console.log('req header', req.headers.host)
+    // const vendor = await $axios.$get(
+    //   `https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin`
+    //   // 'https://api.nuxtjs.dev/mountains'
+    // )
+    // console.log('ip', vendor)
+    // return { vendor }
   }
   // data() {
   //   return {
