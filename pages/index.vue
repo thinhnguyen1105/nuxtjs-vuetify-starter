f991a379542c3c66d31852f4abdd9a58487f1481
Genius Mac <Genius Mac>
2021-07-17 11:44:36 +0700

    :sparkles: update test ui

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
+        <div>{{ siteName }}</div>
+        <img :src="logo" />
         <logo />
         <div>{{ host }}</div>
         <div>{{ vendor }}</div>
@@ -17,23 +19,36 @@ export default {
     Logo
   },
   async asyncData({ req, res, $axios }) {
-    console.log('req header', req.headers.host)
-    const host = req.headers.host
-    const vendor = await $axios.$get(
-      `https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin?hostname=https://${host}`
-    )
-    return { host, vendor }
+    if (req && req.headers && req.headers.host) {
+      const host = req.headers.host
+      const vendor = await $axios.$get(
+        `https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin?hostname=https://${host}`
+      )
+      const settings = await $axios.$get(
+        `https://ilx0gvnp3c.execute-api.ap-southeast-1.amazonaws.com/prod/academic/vendor/${vendor.vendorId}`,
+        {
+          headers: {
+            'ol-client-id': vendor.clientId,
+            'ol-vendor-id': vendor.vendorId
+          }
+        }
+      )
+      const siteName = settings.item.learn.themeConfig.themeData.siteName
+      const logo = settings.item.learn.themeConfig.themeData.logo_url
+
+      return { host, vendor, logo, siteName }
+    }
+    // data() {
+    //   return {
+    //     mountains: []
+    //   }
+    // },
+    // async fetch() {
+    //   this.mountains = await fetch(
+    //     'https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin'
+    //   )
+    //   console.log('this.mountains', this.mountains)
+    // }
   }
-  // data() {
-  //   return {
-  //     mountains: []
-  //   }
-  // },
-  // async fetch() {
-  //   this.mountains = await fetch(
-  //     'https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin'
-  //   )
-  //   console.log('this.mountains', this.mountains)
-  // }
 }
 </script>
