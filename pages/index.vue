0eaad6295a70c8f122f2357cf004d4a95f9c0d5c
Genius Mac <Genius Mac>
2021-07-17 09:32:18 +0700

    test without async data

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
         <logo />
-        {{ vendor }}
       </div>
     </v-col>
   </v-row>
@@ -14,15 +13,26 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
-  },
-  async asyncData({ req, res, $axios }) {
-    if (req) {
-      const vendor = await $axios.$get(
-        `https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin?hostname=https://${req.headers.host}`
-      )
-      console.log('ip', vendor)
-      return { vendor }
-    }
   }
+  // async asyncData({ req, res, $axios }) {
+  //   if (req) {
+  //     const vendor = await $axios.$get(
+  //       `https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin?hostname=https://${req.headers.host}`
+  //     )
+  //     console.log('ip', vendor)
+  //     return { vendor }
+  //   }
+  // }
+  // data() {
+  //   return {
+  //     mountains: []
+  //   }
+  // },
+  // async fetch() {
+  //   this.mountains = await fetch(
+  //     'https://cd30nboy73.execute-api.ap-southeast-1.amazonaws.com/prod/vendor/get-vendor-from-origin'
+  //   )
+  //   console.log('this.mountains', this.mountains)
+  // }
 }
 </script>
