60232115e4eb7d2fe1c5c9b04474b2fc6a413465
Genius Mac <Genius Mac>
2021-07-28 19:25:07 +0700

    test using component in production

--- pages/demo.vue
+++ pages/demo.vue
@@ -7,62 +7,35 @@
       Button
     </button>
     <Dialog>The quick brown fox</Dialog>
-    <v-app>
-      <v-container>
-        <v-row justify="space-around">
-          <v-card width="400">
-            <v-img
-              height="200px"
-              src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
-            >
-              <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
-
-                <v-toolbar-title class="text-h6 white--text pl-0">
-                  Messages
-                </v-toolbar-title>
-
-                <v-spacer></v-spacer>
-
-                <v-btn color="white" icon>
-                  <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
-              </v-app-bar>
-
-              <v-card-title class="white--text mt-8">
-                <v-avatar size="56">
-                  <img
-                    alt="user"
-                    src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
-                  />
-                </v-avatar>
-                <p class="ml-3">John Doe</p>
-              </v-card-title>
-            </v-img>
-
-            <v-card-text>
-              <div class="font-weight-bold ml-8 mb-2">Today</div>
-
-              <v-timeline align-top dense>
-                <v-timeline-item
-                  v-for="message in messages"
-                  :key="message.time"
-                  :color="message.color"
-                  small
-                >
-                  <div>
-                    <div class="font-weight-normal">
-                      <strong>{{ message.from }}</strong> @{{ message.time }}
-                    </div>
-                    <div>{{ message.message }}</div>
-                  </div>
-                </v-timeline-item>
-              </v-timeline>
-            </v-card-text>
-          </v-card>
-        </v-row>
-      </v-container>
-    </v-app>
+    <div
+      class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl"
+    >
+      <div class="md:flex">
+        <div class="md:flex-shrink-0">
+          <img
+            class="h-48 w-full object-cover md:h-full md:w-48"
+            src="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
+            alt="Man looking at item at a store"
+          />
+        </div>
+        <div class="p-8">
+          <div
+            class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"
+          >
+            Case study
+          </div>
+          <a
+            href="#"
+            class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >Finding customers for your new business</a
+          >
+          <p class="mt-2 text-gray-500">
+            Getting a new business off the ground is a lot of hard work. Here
+            are five ideas you can use to find your first customers.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
